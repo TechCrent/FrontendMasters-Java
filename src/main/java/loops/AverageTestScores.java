9df0c38e24f478bcf7007906757356b4e03b0252
TechCrent <ciicicrentsil@gmail.com>
2026-02-01 20:49:06 +0000

    Lesson 10(Methods)

--- src/main/java/loops/AverageTestScores.java
+++ src/main/java/loops/AverageTestScores.java
@@ -1,4 +1,33 @@
 package loops;
 
+import java.util.Scanner;
+
 public class AverageTestScores {
+    public static void main(String[] args){
+
+        //Initialize values
+        int numberOfStudents = 24;
+        int numberOfTests = 4;
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Process all students
+        for(int i = 0; i < numberOfStudents; i++){
+            //initializing total
+            double total = 0;
+
+            //Process students' tests
+            for(int j = 0; j < numberOfTests; j++){
+                System.out.println("Score for Test #" + (j+1));
+                double score = scanner.nextDouble();
+                total = total + score;
+            }
+
+            double average = total/numberOfTests;
+            System.out.println("The test average for student #" + (i+1) + "is" + average);
+        }
+
+        scanner.close();
+
+    }
 }
---
+++ src/main/java/methods/Sum.java
@@ -0,0 +1,10 @@
+package methods;
+
+public class Sum {
+
+    public static int calculateSum(int number1, int number2){
+
+        int sum = number1 + number2;
+        return sum;
+    }
+}
